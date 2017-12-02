1ee6fb8c32e69195cd69415d96f463a343d422ad
Max Fulgoni <fulgonim@gmail.com>
2017-12-01 23:04:32 -0500

    page functional, needs design help

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const YOUTUBE_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search";
-const KEY = "AIzaSyAl6ccLDZMyS0A29z-jG4HwaO8e1OMAXkQ192";
+const KEY = "AIzaSyAl6ccLDZMyS0A29z-jG4HwaO8e1OMAXkQ";
 
 
 
@@ -8,33 +8,62 @@ function getDataFromApi(searchTerm, callback) {
 	//Thumbnail image, Title, Name of uploader, date uploaded, brief description?
 
 	const settings = {
+        url: YOUTUBE_SEARCH_URL,
+        data: {
+            part: 'snippet',
+            key: KEY,
+            q: searchTerm
+    },
+    	jsonp: 'callback',
+        dataType: 'jsonp',
+        type: 'GET',
+        success: function (data) {
+     				 console.log("success", data);
+     			displayYoutubeSearchData(data);
+    },
+        error: function () {
+            alert("Error loading video");
+        }
+        
+
+    };
+    $.ajax(settings);
+}
+	/*const settings = {
 		url: YOUTUBE_SEARCH_URL,
 		data: {
 			part: 'snippet',
 			key: KEY,
-			q: searchTerm
+			q: searchTerm,
+			per_page: 5
 		},
-		dataType: 'json',
+		dataType: 'jsonp',
+		jsonp: 'callback',
 		type: 'GET',
 		success: callback,
-		error: errorFunction() {
-			alert("Error loading video")
-		};,
+		/*error: errorFunction() {
+					alert("Error loading video");
+				}		
 		*/
+/*
 
 	};
 	$.ajax(settings);
-}
 
+
+}
+*/
 
 function renderResult(result) {
 	//create a new div element using the data from the API call REMEMBER: this is just one item
 
 	return `
-		<div>
-			<span class="js-result-title">${result.snippet.title}</span>
-			<span class="js-result-description">${result.snippet.description}</span>
-			<span class="js-result-published">${result.snippet.publishedAt}</span>
+		<div class="css-search-result">
+			<span class="js-results-thumbnail">
+				<a href="https://www.youtube.com/watch?v=${result.id.videoId}" target="_blank"><img src="${result.snippet.thumbnails.default.url}" alt="${result.snippet.title}">
+			</span></a>
+			<span class="js-result-title css-result-title"><a href="https://www.youtube.com/watch?v=${result.id.videoId}" target="_blank">${result.snippet.title}</a></span>
+			<span class="js-result-description css-result-description">${result.snippet.description}</span>
 		</div> 
 		`;
 }
